0bc0a26884f001dc1e001a9403d2e127758fa6ab
Eddy-Singh <appsingh@mun.ca>
2024-04-28 10:21:54 -0230

    Starting with pattern logic

---
+++ Patterns/patterns.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cctype>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << "Pattern 1: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 2: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 3: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 4: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 5: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 6: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 7: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 8: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 9: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 10: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 11: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 12: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 13: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 14: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 15: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 16: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 17: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 18: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 19: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 20: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 21: " << endl;
+    cout << endl
+         << endl;
+    cout << "Pattern 22: " << endl;
+    cout << endl
+         << endl;
+
+    return 0;
+}
