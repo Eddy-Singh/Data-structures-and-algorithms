f6d96b4b791a72acf74956ee410e4cd1a7156d37
Eddy-Singh <appsingh@mun.ca>
2024-05-04 10:51:49 -0230

    Armstrong number verification

--- Math problems/math_problems.cpp
+++ Math problems/math_problems.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 // Find the number of digits of ‘n’ that evenly divide ‘n’.
@@ -26,22 +27,12 @@ int countDigits(int n)
 // There is a song concert going to happen in the city. The price of each ticket is equal to the number obtained after reversing the bits of a given 32 bits unsigned integer ‘n’.
 long reverseBits(long n)
 {
-    // Initialize result to 0
     long result = 0;
-
-    // Iterate through each bit of the given number
     for (int i = 0; i < 32; ++i)
     {
-        // Get the least significant bit of n
         int bit = n & 1;
-
-        // Shift the result to the left by 1 bit
         result <<= 1;
-
-        // Add the current bit to the result
         result |= bit;
-
-        // Right shift n by 1 bit to process the next bit
         n >>= 1;
     }
 
@@ -49,9 +40,46 @@ long reverseBits(long n)
     return result;
 }
 
+int calcGCD(int n, int m)
+{
+    int j = 0;
+    for (int i = 1; i <= min(n, m); i++)
+    {
+        if (min(n, m) % i == 0 && (max(n, m) % i) == 0)
+            j = i;
+    }
+    return j;
+}
+
+// You are given an integer 'n'. Return 'true' if 'n' is an Armstrong number, and 'false' otherwise.
+
+// An Armstrong number is a number (with 'k' digits) such that the sum of its digits raised to 'kth' power is equal to the number itself. For example, 371 is an Armstrong number because 3^3 + 7^3 + 1^3 = 371.
+bool checkArmstrong(int n)
+{
+    int dup = n;
+    int dig = 0;
+    while (dup > 0)
+    {
+        dup = dup / 10;
+        dig++;
+    }
+    int sum = 0;
+    int dup2 = n;
+    int temp = 0;
+    while (dup2 > 0)
+    {
+        temp = dup2 % 10;
+        sum = sum + pow(temp, dig);
+        dup2 = dup2 / 10;
+    }
+    return (n == sum);
+}
+
 int main()
 {
     cout << countDigits(336) << endl;
     cout << reverseBits(12) << endl;
+    cout << calcGCD(20, 15) << endl;
+    cout << checkArmstrong(371) << endl;
     return 0;
 }
