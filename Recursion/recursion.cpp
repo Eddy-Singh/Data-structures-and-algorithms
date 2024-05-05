29f9eaad7a521771b80fd63f3fff7c67c6cc50d5
Eddy-Singh <appsingh@mun.ca>
2024-05-05 10:38:48 -0230

    recursion problem #2

--- Recursion/recursion.cpp
+++ Recursion/recursion.cpp
@@ -11,6 +11,14 @@ vector<int> printNos(int x, vector<int> v = {})
     return v;
 }
 
+vector<string> printNTimes(int n, vector<string> v = {})
+{
+    if (n == 0)
+        return v;
+    v.emplace_back("Hello");
+    return printNTimes(n - 1, v);
+}
+
 int main()
 {
     auto j = printNos(5);
@@ -18,5 +26,12 @@ int main()
     {
         cout << i << " ";
     }
+
+    auto p = printNTimes(5);
+    for (auto i : p)
+    {
+        cout << i << " ";
+    }
+
     return 0;
 }
