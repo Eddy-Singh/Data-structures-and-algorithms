3d594381bb1b01501d56e73686f6c698e33b8c93
Eddy-Singh <appsingh@mun.ca>
2024-05-07 11:44:41 -0230

    number mapping

--- Hash Maps/hashing.cpp
+++ Hash Maps/hashing.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 
 int main()
-{
+{ // Using hashing to store the frequency of numbers in an array
     int n;
     cout << "enter the size of your array: ";
-
     cin >> n;
     cout << endl;
     int arr[n];
@@ -35,6 +34,7 @@ int main()
         cout << "Frequency: " << hash[x] << endl;
     }
 
+    // Using hashing to store the frequency of characters in a string
     string s;
     cout << "enter your string: ";
     cin >> s;
@@ -45,7 +45,6 @@ int main()
     {
         hashh[s[i] - 'a']++;
     }
-
     // fetching
     int k;
     cout << "enter the number of characters you want to fetch: ";
@@ -57,6 +56,31 @@ int main()
         cout << "Enter character: ";
         cin >> c;
         cout << "Frequency: " << hashh[c - 'a'] << endl;
+    }
+
+    int t;
+    cin >> t;
+    int arrr[t];
+    for (int i = 0; i < t; i++)
+    {
+        cin >> arrr[i];
+    }
+
+    // precomputing
+    map<int, int> mpp;
+    for (int i = 0; i < t; i++)
+    {
+        mpp[arrr[i]]++;
+    }
+
+    int y;
+    cout << "enter the number of elements you want to fetch: ";
+    cin >> y;
+    while (y--)
+    {
+        int x;
+        cin >> x;
+        cout << "Frequency: " << mpp[x] << endl;
     }
 
     return 0;
