f56349ba0395f174abb674cb8b91226c8462036b
Eddy-Singh <appsingh@mun.ca>
2024-05-08 11:40:30 -0230

    hash map problem #2

--- Hash Maps/hash_maps_problems.cpp
+++ Hash Maps/hash_maps_problems.cpp
@@ -21,6 +21,31 @@ int main()
     {
         cout << i.first << " " << i.second << endl;
     }
+    cout << endl
+         << endl;
 
+    // Given an array of size N. Find the highest and lowest frequency element.
+    int maxFreq = 0, minFreq = a;
+    int maxEle = 0, minEle = 0;
+    // Traverse through map to find the elements.
+    for (auto it : mpp)
+    {
+        int count = it.second;
+        int element = it.first;
+
+        if (count > maxFreq)
+        {
+            maxEle = element;
+            maxFreq = count;
+        }
+        if (count < minFreq)
+        {
+            minEle = element;
+            minFreq = count;
+        }
+    }
+
+    cout << "The highest frequency element is: " << maxEle << "\n";
+    cout << "The lowest frequency element is: " << minEle << "\n";
     return 0;
 }
