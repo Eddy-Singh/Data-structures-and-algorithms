99c1450ed09746137d59019c71f4486d8cd6cf17
Eddy-Singh <appsingh@mun.ca>
2024-05-13 13:41:10 -0230

    Recursive bubble sort

--- Sorting/Sorting-II.cpp
+++ Sorting/Sorting-II.cpp
@@ -1,19 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(vector<int> &arr, int low, int mid, int high) {
-    vector<int> temp; // temporary array
+void merge(vector<int> &arr, int low, int mid, int high)
+{
+    vector<int> temp;    // temporary array
     int left = low;      // starting index of left half of arr
-    int right = mid + 1;   // starting index of right half of arr
+    int right = mid + 1; // starting index of right half of arr
 
-    //storing elements in the temporary array in a sorted manner//
+    // storing elements in the temporary array in a sorted manner//
 
-    while (left <= mid && right <= high) {
-        if (arr[left] <= arr[right]) {
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
             temp.push_back(arr[left]);
             left++;
         }
-        else {
+        else
+        {
             temp.push_back(arr[right]);
             right++;
         }
@@ -21,46 +25,72 @@ void merge(vector<int> &arr, int low, int mid, int high) {
 
     // if elements on the left half are still left //
 
-    while (left <= mid) {
+    while (left <= mid)
+    {
         temp.push_back(arr[left]);
         left++;
     }
 
     //  if elements on the right half are still left //
-    while (right <= high) {
+    while (right <= high)
+    {
         temp.push_back(arr[right]);
         right++;
     }
 
     // transfering all elements from temporary to arr //
-    for (int i = low; i <= high; i++) {
+    for (int i = low; i <= high; i++)
+    {
         arr[i] = temp[i - low];
     }
 }
 
-void mergeSort(vector<int> &arr, int low, int high) {
-    if (low >= high) return;
-    int mid = (low + high) / 2 ;
-    mergeSort(arr, low, mid);  // left half
+void mergeSort(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);      // left half
     mergeSort(arr, mid + 1, high); // right half
-    merge(arr, low, mid, high);  // merging sorted halves
+    merge(arr, low, mid, high);    // merging sorted halves
 }
 
-int main() {
+void recursiveBubbleSort(int arr[], int n)
+{
+    if (n == 1)
+        return;
+    int didSwap = 0;
+    for (int i = 0; i < n - 2; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            swap(arr[i], arr[i + 1]);
+            didSwap = 1;
+        }
+    }
+    if (didSwap == 0)
+        return;
+    recursiveBubbleSort(arr, n - 1);
+}
 
-    vector<int> arr = {9, 4, 7, 6, 3, 1, 5}  ;
+int main()
+{
+
+    vector<int> arr = {9, 4, 7, 6, 3, 1, 5};
     int n = 7;
 
     cout << "Before Sorting Array: " << endl;
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " "  ;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
     cout << endl;
     mergeSort(arr, 0, n - 1);
     cout << "After Sorting Array: " << endl;
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " "  ;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
     cout << endl;
-    return 0 ;
+    return 0;
 }
