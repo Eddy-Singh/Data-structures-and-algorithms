848f5302bbb7d5c748a828eee17a9e48b989fcae
Eddy-Singh <appsingh@mun.ca>
2024-08-24 13:26:23 -0230

    revision

--- Arrays/Array_I.cpp
+++ Arrays/Array_I.cpp
@@ -20,7 +20,7 @@ int findmax2(auto arr)
     return arr[arr.size() - 2];
 }
 
-// check if array is sorted
+// 3 check if array is sorted
 bool isSorted(int arr[], int n)
 {
     for (int i = 1; i < n; i++)
@@ -32,12 +32,28 @@ bool isSorted(int arr[], int n)
     return true;
 }
 
+// 4 remove duplicates from an array
+void removeDuplicates(int arr[], int n)
+{
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j)
+        {
+            if (arr[i] == arr[j])
+            {
+            }
+        }
+    }
+}
+
 int main()
 {
     int arr[] = {2, 5, 14, 2, 5, 16, 4, 23, 6, 18};
     vector<int> arr2 = {2, 5, 14, 2, 5, 16, 4, 23, 6, 18};
     int i = findmax(arr, sizeof(arr) / sizeof(arr[0]));
     int j = findmax2(arr2);
+    removeDuplicates(arr2, sizeof(arr2) / sizeof(arr2[0]));
     cout << i << endl;
     cout << j << endl;
     return 0;
