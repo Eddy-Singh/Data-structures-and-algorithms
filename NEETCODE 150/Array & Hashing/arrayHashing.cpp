a51675ae3235d00db67c51133589ef2e2875267d
Eddy-Singh <appsingh@mun.ca>
2024-09-09 10:57:27 -0230

    Refactor array&Hashing.cpp file for NEETCODE 150

---
+++ NEETCODE 150/Array & Hashing/arrayHashing.cpp
@@ -0,0 +1,36 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    // 1. Duplicate Integer
+    bool hasDuplicate(vector<int> &nums)
+    { // bruteforce
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    bool hasDuplicateAlternate(vector<int> &nums)
+    { // good solution
+        unordered_set<int> s;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            s.emplace(nums[i]);
+        }
+        return (s.size() != nums.size()) ? true : false;
+    }
+};
+int main()
+{
+
+    return 0;
+}
