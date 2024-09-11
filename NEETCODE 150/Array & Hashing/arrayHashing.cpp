50ced99cf7eed5ac3a89af247676d60082ce6f93
Eddy-Singh <appsingh@mun.ca>
2024-09-10 23:30:24 -0230

    Refactor arrayHashing.cpp to add a twoSum function for finding pairs that sum up to a target

--- NEETCODE 150/Array & Hashing/arrayHashing.cpp
+++ NEETCODE 150/Array & Hashing/arrayHashing.cpp
@@ -59,6 +59,19 @@ public:
 
         return false;
     }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> mp; 
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+            mp.insert({nums[i], i});
+        }
+        return {};
+    }
 };
 int main()
 {
