e6da22b539b3e0d4930141b28e9e1f0b7598cd78
Eddy-Singh <appsingh@mun.ca>
2024-11-23 19:43:49 -0330

    Add isValid function to stack class for validating parentheses

--- NEETCODE 150/Array & Hashing/stack.cpp
+++ NEETCODE 150/Array & Hashing/stack.cpp
@@ -3,4 +3,26 @@ using namespace std;
 class Solution
 {
 public:
+    bool isValid(string s)
+    {
+        stack<int> st;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                st.emplace(s[i]);
+
+            else
+            {
+                if (st.empty())
+                    return false;
+                char c = st.top();
+                if ((s[i] == ')' && c != '(') ||
+                    (s[i] == '}' && c != '{') ||
+                    (s[i] == ']' && c != '['))
+                    return false;
+                st.pop();
+            }
+        }
+        return true;
+    }
 };
