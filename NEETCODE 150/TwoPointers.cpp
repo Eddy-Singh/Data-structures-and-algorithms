fd0761c8a28d73b1368418b7da44c68b1337f37c
Eddy-Singh <appsingh@mun.ca>
2024-11-25 12:35:02 -0330

    Add twoSum function to Solution class in TwoPointers.cpp

--- NEETCODE 150/TwoPointers.cpp
+++ NEETCODE 150/TwoPointers.cpp
@@ -20,4 +20,23 @@ public:
         }
         return true;
     }
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+        int l = 0;
+        int r = numbers.size() - 1;
+
+        while (l < r)
+        {
+
+            if (numbers[l] + numbers[r] > target)
+                r--;
+
+            if (numbers[l] + numbers[r] < target)
+                l++;
+
+            if (numbers[l] + numbers[r] == target)
+                break;
+        }
+        return {l + 1, r + 1};
+    }
 };
